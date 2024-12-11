5d9c9042a9331f364c9388772cdb821a2337690f
Uoi5553e <76123699+YouGold@users.noreply.github.com>
2024-12-11 11:40:10 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 var message_box = document.getElementById("message_box")
 var send_message = document.getElementById("send_message")
+var message_section = document.getElementById("message_section")
 
 send_message.onclick = function() 
 {
  if (message_box.value.length > 0) 
     {
-        window.alert(`You clicked me :< ${String(2)}`) 
+        const message_form = document.createElement("figure");
+        const message_text = document.createElement("p");
+
+        message_form.classList.add("message_container");
+        message_section.appendChild(message_form);
+        message_form.appendChild(message_text);
+
+        message_text.textContent = message_box.value
+
     }
 
-    for (let index = 0; index < 10; index++) {
-        
-    }
 }
