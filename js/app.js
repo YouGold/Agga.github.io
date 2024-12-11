8a5702ed05004ba02eb2b88ad9b0ad42ed87418d
Uoi5553e <76123699+YouGold@users.noreply.github.com>
2024-12-11 12:11:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,16 +6,36 @@ send_message.onclick = function()
 {
  if (message_box.value.length > 0) 
     {
-        const message_form = document.createElement("figure");
-        const message_text = document.createElement("p");
 
-        message_form.classList.add("message_container");
-        message_section.appendChild(message_form);
-        message_form.appendChild(message_text);
-
-        message_text.textContent = message_box.value
-        message_box.value = ""
+        Add_New_Message_Box()
 
     }
 
+}
+
+function Add_New_Message_Box() 
+{
+    const message_form = document.createElement("figure");
+    const message_text = document.createElement("p");
+    const message_remove_button = document.createElement("input");
+    message_remove_button.setAttribute("type","button");
+
+    message_form.classList.add("message_container");
+    message_section.appendChild(message_form);
+    message_form.appendChild(message_remove_button);
+    message_form.appendChild(message_text);
+        
+    message_text.textContent = message_box.value
+    message_box.value = ""
+    message_remove_button.value = "X"
+
+    Remove_Box_Handler(message_remove_button, message_form)
+}
+
+function Remove_Box_Handler(message_remove_button, message_form) 
+{
+    message_remove_button.onclick = function() 
+    {
+        message_form.remove();
+    }
 }
